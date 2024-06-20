26af162726fa990058eccad1a7b012ec90fff4c5
Anthony Martini <anthonycm2022@gmail.com>
2024-06-19 22:20:52 -0400

    Changes

--- client/src/components/Workout.jsx
+++ client/src/components/Workout.jsx
@@ -5,8 +5,7 @@ export default function Workout() {
         <div className='m-2 w-[600px] h-[600px] bg-slate-100 rounded-[20px] drop-shadow-xl'>
 
             <div className="w-full h-full p-2">
-                <p className='font-bold text-stone-700 text-[24px] text-center w-full'> Today's Workout</p>
-
+                <p className='font-bold text-stone-700 text-[24px] text-center w-full'> Today&apos;s Workout</p>
                 <div>
                     Buttons Go Here
                 </div>
